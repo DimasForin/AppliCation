906f2fef4d9db8961f6b6e72c0e3b6e291d2bb1d
Lada <lada.spectorovska@gmail.com>
2021-02-02 18:38:57 +0200

    пьвалдотвадпльдальпдльвадльвадлпьдвльапльвадпьдвальп

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,9 +14,6 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 Button myStartButton;
 TextView myTextOne;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
