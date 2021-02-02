576562e774ef09a59c47645988133cda7a6f5de9
DimasForin <dforin2005@gmail.com>
2021-02-02 18:02:15 +0300

    1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+Button myStartButton;
+TextView myTextOne;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myTextOne=findViewById(R.id.textView8);
+        myStartButton=findViewById(R.id.button);
+        myTextOne.setMovementMethod(new ScrollingMovementMethod());
+        myStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Игра находится в разработке", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
